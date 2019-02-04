5ab042ed1cf2af0410a26f82457494b945938b54
unknown <raphael.castilhoc@gmail.com>
2019-02-03 23:30:13 -0200

    primeiro commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+from resources.student_resource import StudentResource
+
+
+app = Flask(__name__)
+app.secret_key = 'raphael'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(StudentResource, '/student/<string:name>')
+
+app.run(port=5000, debug=True)
